80ddea3b093362bc1aad3d5fdda98cacab96ee2c
Kamil <Kamil@MacBook-Pro.local>
2020-08-17 18:25:48 +0200

    Updates

---
+++ precache-manifest.8106a103d74d4c0548971835666a2efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09de95467add12bd7e3d8c33051e2067",
+    "url": "/BartekPanel/index.html"
+  },
+  {
+    "revision": "0d126b243e40e83e64df",
+    "url": "/BartekPanel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73fa8c68d8cae5cf8a6c",
+    "url": "/BartekPanel/static/js/2.2038d2ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BartekPanel/static/js/2.2038d2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d126b243e40e83e64df",
+    "url": "/BartekPanel/static/js/main.40152af9.chunk.js"
+  },
+  {
+    "revision": "6be4e2e9a5307d861941",
+    "url": "/BartekPanel/static/js/runtime-main.9103932b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BartekPanel/static/media/logo.5d5d9eef.svg"
+  }
+]);
